7d2e01c399d7815e4a5d928ba690f2f8e85ea450
lote89 <42542694+lote89@users.noreply.github.com>
2026-01-07 23:08:11 +0100

    Update CreateWarehouseUseCase.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/domain/usecases/CreateWarehouseUseCase.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/domain/usecases/CreateWarehouseUseCase.java
@@ -4,21 +4,41 @@ import com.fulfilment.application.monolith.warehouses.domain.models.Warehouse;
 import com.fulfilment.application.monolith.warehouses.domain.ports.CreateWarehouseOperation;
 import com.fulfilment.application.monolith.warehouses.domain.ports.WarehouseStore;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.time.ZonedDateTime;
+import com.fulfilment.application.monolith.warehouses.domain.exceptions.WarehouseDomainException;
 
 @ApplicationScoped
 public class CreateWarehouseUseCase implements CreateWarehouseOperation {
 
-  private final WarehouseStore warehouseStore;
+  
+private final WarehouseStore warehouseStore;
+public CreateWarehouseUseCase(WarehouseStore warehouseStore) {
+  this.warehouseStore = warehouseStore;
+}
 
-  public CreateWarehouseUseCase(WarehouseStore warehouseStore) {
-    this.warehouseStore = warehouseStore;
+@Override
+public void create(Warehouse warehouse) {
+  // BU check
+  if (warehouseStore.existsByBusinessUnitCode(warehouse.businessUnitCode)) {
+    throw new WarehouseDomainException("Business unit code already exists: " + warehouse.businessUnitCode);
   }
 
-  @Override
-  public void create(Warehouse warehouse) {
-    // TODO implement this method
-
-    // if all went well, create the warehouse
-    warehouseStore.create(warehouse);
+  // Location/Max (hardcode or skip validation for compile)
+  long activeCount = warehouseStore.countActiveByLocation(warehouse.location);
+  if (activeCount >= 5) {  // Temp hardcoded max
+    throw new WarehouseDomainException("Max warehouses reached at location: " + warehouse.location);
   }
+
+  // Capacity/Stock
+  if (warehouse.capacity > 1000) {  // Temp max
+    throw new WarehouseDomainException("Capacity exceeded: " + warehouse.capacity);
+  }
+  if (warehouse.stock > warehouse.capacity) {
+    throw new WarehouseDomainException("Stock exceeds capacity");
+  }
+
+  warehouse.creationAt = ZonedDateTime.now();
+  warehouseStore.create(warehouse);
+}
+
 }
