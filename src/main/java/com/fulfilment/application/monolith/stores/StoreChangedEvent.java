6323a880d7fd02cd1e9f708fa342c04ad56bb80d
lote89 <42542694+lote89@users.noreply.github.com>
2026-01-07 15:14:10 +0100

    Update StoreChangedEvent.java

--- src/main/java/com/fulfilment/application/monolith/stores/StoreChangedEvent.java
+++ src/main/java/com/fulfilment/application/monolith/stores/StoreChangedEvent.java
@@ -1,1 +1,7 @@
+package com.fulfilment.application.monolith.stores;
 
+import com.fulfilment.application.monolith.stores.domain.Store;
+
+public record StoreChangedEvent(Store store, Operation operation) {}
+
+enum Operation { CREATED, UPDATED, DELETED }
