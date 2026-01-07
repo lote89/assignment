5e447da343cf79d1473980cec15c4f0d0375e10c
lote89 <42542694+lote89@users.noreply.github.com>
2026-01-07 23:04:16 +0100

    Update DbWarehouse.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DbWarehouse.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DbWarehouse.java
@@ -1,30 +1,21 @@
 package com.fulfilment.application.monolith.warehouses.adapters.database;
 
-import jakarta.persistence.Cacheable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import java.time.ZonedDateTime;  // Match domain
 
 @Entity
 @Table(name = "warehouse")
 @Cacheable
 public class DbWarehouse {
-
-  @Id @GeneratedValue public Long id;
+  @Id @GeneratedValue 
+  public Long id;
 
   public String businessUnitCode;
-
   public String location;
-
   public Integer capacity;
-
   public Integer stock;
-
-  public LocalDateTime createdAt;
-
-  public LocalDateTime archivedAt;
+  public ZonedDateTime creationAt;  // Match domain
+  public ZonedDateTime archivedAt;
 
   public DbWarehouse() {}
 }
