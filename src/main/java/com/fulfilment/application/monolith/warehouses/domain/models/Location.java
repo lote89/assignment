e64f9c5c71b1bd1f37b72877dc72665d67183683
lote89 <42542694+lote89@users.noreply.github.com>
2026-01-07 15:54:08 +0100

    Update Location.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/domain/models/Location.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/domain/models/Location.java
@@ -1,3 +1,17 @@
 package com.fulfilment.application.monolith.warehouses.domain.models;
 
-public record Location(String identifier, int maxWarehouses, int maxCapacity) {}
+public class Location {
+  public String identification;
+
+  // maximum number of warehouses that can be created in this location
+  public int maxNumberOfWarehouses;
+
+  // maximum capacity of the location summing all the warehouse capacities
+  public int maxCapacity;
+
+  public Location(String identification, int maxNumberOfWarehouses, int maxCapacity) {
+    this.identification = identification;
+    this.maxNumberOfWarehouses = maxNumberOfWarehouses;
+    this.maxCapacity = maxCapacity;
+  }
+}
