070b11ebe5aba6a2270140e2821ecd5479720b2e
lote89 <42542694+lote89@users.noreply.github.com>
2026-01-07 23:08:31 +0100

    Update ReplaceWarehouseUseCase.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/domain/usecases/ReplaceWarehouseUseCase.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/domain/usecases/ReplaceWarehouseUseCase.java
@@ -4,20 +4,35 @@ import com.fulfilment.application.monolith.warehouses.domain.models.Warehouse;
 import com.fulfilment.application.monolith.warehouses.domain.ports.ReplaceWarehouseOperation;
 import com.fulfilment.application.monolith.warehouses.domain.ports.WarehouseStore;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.time.ZonedDateTime;
+import com.fulfilment.application.monolith.warehouses.domain.exceptions.WarehouseDomainException;
 
 @ApplicationScoped
 public class ReplaceWarehouseUseCase implements ReplaceWarehouseOperation {
-
   private final WarehouseStore warehouseStore;
+  private final ArchiveWarehouseUseCase archiveUseCase;
 
-  public ReplaceWarehouseUseCase(WarehouseStore warehouseStore) {
+  public ReplaceWarehouseUseCase(WarehouseStore warehouseStore, ArchiveWarehouseUseCase archiveUseCase) {
     this.warehouseStore = warehouseStore;
+    this.archiveUseCase = archiveUseCase;
   }
 
   @Override
-  public void replace(Warehouse newWarehouse) {
-    // TODO implement this method
-
-    warehouseStore.update(newWarehouse);
+public void replace(Warehouse newWarehouse) {
+  Warehouse oldWarehouse = warehouseStore.findByBusinessUnitCode(newWarehouse.businessUnitCode);
+  if (oldWarehouse == null) {
+    throw new WarehouseDomainException("Warehouse not found: " + newWarehouse.businessUnitCode);
   }
+
+  if (newWarehouse.capacity < oldWarehouse.stock) {
+    throw new WarehouseDomainException("Capacity cannot accommodate stock: " + newWarehouse.capacity);
+  }
+  if (!newWarehouse.stock.equals(oldWarehouse.stock)) {
+    throw new WarehouseDomainException("Stock mismatch: " + newWarehouse.stock);
+  }
+
+  archiveUseCase.archive(oldWarehouse);
+  newWarehouse.creationAt = ZonedDateTime.now();
+  warehouseStore.update(newWarehouse);
+}
 }
