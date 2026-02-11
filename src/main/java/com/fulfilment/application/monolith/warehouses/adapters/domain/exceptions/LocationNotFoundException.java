d1088dd9cf43bc85a075e5777ad7f450e8e5b54f
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-11 17:13:48 +0100

    Update LocationNotFoundException.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/domain/exceptions/LocationNotFoundException.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/domain/exceptions/LocationNotFoundException.java
@@ -1,4 +1,4 @@
-package com.fulfilment.application.monolith.warehouse.adapters.domain.exceptions;
+package com.fulfilment.application.monolith.warehouses.adapters.domain.exceptions;
 
 public class LocationNotFoundException extends WarehouseDomainException {
     public LocationNotFoundException(String message) {
