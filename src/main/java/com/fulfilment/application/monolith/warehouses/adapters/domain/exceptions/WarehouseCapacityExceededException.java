64cad9d1ce02faa6d61d273248d9a3e69089d518
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-11 17:14:19 +0100

    Update WarehouseCapacityExceededException.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/domain/exceptions/WarehouseCapacityExceededException.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/domain/exceptions/WarehouseCapacityExceededException.java
@@ -1,4 +1,4 @@
-package com.fulfilment.application.monolith.warehouse.adapters.domain.exceptions;
+package com.fulfilment.application.monolith.warehouses.adapters.domain.exceptions;
 
 public class WarehouseCapacityExceededException extends WarehouseDomainException {
     public WarehouseCapacityExceededException(String message) {
