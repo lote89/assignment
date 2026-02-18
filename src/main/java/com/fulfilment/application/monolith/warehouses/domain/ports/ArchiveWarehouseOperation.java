68c6e7589029c3ca3a2c004fa94a1d22fb36a8af
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-18 18:33:25 +0100

    Update ArchiveWarehouseOperation.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/domain/ports/ArchiveWarehouseOperation.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/domain/ports/ArchiveWarehouseOperation.java
@@ -1,7 +1,7 @@
 package com.fulfilment.application.monolith.warehouses.domain.ports;
 
-import com.fulfilment.application.monolith.warehouses.domain.models.Warehouse;
+import com.fulfilment.application.monolith.warehouses.domain.models.DomainWarehouse;
 
 public interface ArchiveWarehouseOperation {
-  void archive(Warehouse warehouse);
+  void archive(DomainWarehouse warehouse);
 }
