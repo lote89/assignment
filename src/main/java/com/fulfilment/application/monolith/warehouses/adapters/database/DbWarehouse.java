30d5ef30bc5364553c30c7142761786ab44de334
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-18 15:07:51 +0100

    Update DbWarehouse.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DbWarehouse.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DbWarehouse.java
@@ -43,7 +43,7 @@ public class DbWarehouse extends PanacheEntity {
     
     public Warehouse toDomain() {
         return new Warehouse(
-            this.getId(),  
+            db.getId(),  
             businessUnitCode,
             location,
             capacity,
