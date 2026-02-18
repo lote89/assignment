0b6c30b8a42a60e5192f39259a26b877708bb548
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-18 18:34:53 +0100

    Update WarehouseStore.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/domain/ports/WarehouseStore.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/domain/ports/WarehouseStore.java
@@ -1,19 +1,19 @@
 package com.fulfilment.application.monolith.warehouses.domain.ports;
 
-import com.fulfilment.application.monolith.warehouses.domain.models.Warehouse;
+import com.fulfilment.application.monolith.warehouses.domain.models.DomainWarehouse;
 import java.util.List;
 import java.util.Optional;
 
 public interface WarehouseStore {
-  void create(Warehouse warehouse);
+  void create(DomainWarehouse warehouse);
 
-  void update(Warehouse warehouse);
+  void update(DomainWarehouse warehouse);
 
   void archive(Long id);
 
   Warehouse findByBusinessUnitCode(String buCode);
   boolean existsByBusinessUnitCode(String code);
   long countActiveByLocation(String location);
-  List<Warehouse> findAllActive();
-  Optional<Warehouse> findById(Long id);
+  List<DomainWarehouse> findAllActive();
+  Optional<DomainWarehouse> findById(Long id);
 }
