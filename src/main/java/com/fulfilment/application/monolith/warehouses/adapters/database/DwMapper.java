eddc55bf4e0d86c3640d747cef8590f9157de54f
lote89 <42542694+lote89@users.noreply.github.com>
2026-02-19 08:47:38 +0100

    Update DwMapper.java

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DwMapper.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/database/DwMapper.java
@@ -5,9 +5,16 @@ import java.time.ZonedDateTime;
 
 public class DwMapper {
     public static DomainWarehouse toDomain(DbWarehouse db) {
-        return new DomainWarehouse(
-            db.id, db.businessUnitCode, db.location, db.capacity, 
-            db.stock, db.creationAt, db.archivedAt
-        );
-    }
+    return new DomainWarehouse(
+        db.id.toString(),  // Long → String
+        db.businessUnitCode,
+        db.location,
+        db.capacity,
+        db.stock,
+        db.creationAt,
+        db.archivedAt
+    );
+}
+
+    
 }
